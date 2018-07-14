50060c29a9a0d09024e44080b1501cc706e52dd9
Beta Faccion <betafcc@gmail.com>
2018-07-14 17:03:18 -0300

    Adds asynctools.sync

--- misc/asynctools.py
+++ misc/asynctools.py
@@ -1,4 +1,16 @@
+import asyncio
+from functools import wraps
 from collections.abc import Awaitable
+
+
+def sync(f, loop=None):
+    @wraps(f)
+    def _sync(*args, **kwargs):
+        coro = f(*args, **kwargs)
+        if loop is None:
+            return asyncio.run(coro)
+        return loop.run_until_complete(coro)
+    return _sync
 
 
 async def resolve(awaitable_or_not):
