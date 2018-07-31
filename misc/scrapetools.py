176f3681334f3c1ccba4d685e0c4d3b875c66e53
Beta Faccion <betafcc@gmail.com>
2018-07-31 07:23:10 -0300

    Adds simpler asyncio.request

--- misc/scrapetools.py
+++ misc/scrapetools.py
@@ -1,9 +1,8 @@
-import asyncio
 import collections.abc
 from itertools import cycle
+from functools import partial
 from types import SimpleNamespace
 
-import tqdm
 import aiohttp
 import dateparser
 from parsel import Selector
@@ -18,7 +17,7 @@ async def scrape(url, *args, **kwargs):
         if 'session' not in kwargs:
             kwargs.update(session={'raise_for_status': True})
 
-        response = await get_one(url, *args, **kwargs)
+        response = await get(url, *args, **kwargs)
         html = response.body.decode(response.charset)
 
         return Selector(text=html, base_url=url)
@@ -30,75 +29,21 @@ async def scrape(url, *args, **kwargs):
             return Selector(text=url.body.decode(url.charset))
 
 
-@agnostic
-async def get(urls, *args, **kwargs):
-    if isinstance(urls, str):
-        return await get_one(urls, *args, **kwargs)
-    return await get_all(urls, *args, **kwargs)
-
-
-async def get_one(*args, session=None, **kwargs):
+async def request(method, url, *args, session=None, **kwargs):
     if session is None:
         session = {}
-
     if isinstance(session, dict):
         async with aiohttp.ClientSession(**session) as session:
-            return await get_one(*args, session=session, **kwargs)
-
-    async with session.get(*args, **kwargs) as response:
-        try:
-            response.body = await response.read()
-            response.sucess = True
-        except Exception as exception:
-            response.exception = exception
-            response.sucess = False
+            return await request(method, url, *args, session=session, **kwargs)
+    async with session.request(method, url, *args, **kwargs) as response:
+        await response.read()
         return response
 
 
-async def get_all(urls,
-                  *args,
-                  session=None,
-                  show_progress=False,
-                  pbar=None,
-                  **kwargs,
-                  ):
-    if session is None:
-        session = {}
-
-    if isinstance(session, dict):
-        if 'connector' not in session:
-            _len = len(urls)
-            connector = aiohttp.TCPConnector(limit=_len, limit_per_host=_len)
-            session = {'connector': connector, **session}
-
-        async with aiohttp.ClientSession(**session) as session:
-            return await get_all(
-                urls,
-                *args,
-                session=session,
-                show_progress=show_progress,
-                pbar=pbar,
-                **kwargs,
-            )
-
-    if show_progress and pbar is None:
-        with tqdm.tqdm(total=len(urls)) as pbar:
-            return await get_all(
-                urls,
-                *args,
-                session=session,
-                show_progress=show_progress,
-                pbar=pbar,
-                **kwargs,
-            )
-
-    async def mapper(url):
-        result = await get_one(url, *args, session=session, **kwargs)
-        if show_progress:
-            pbar.update(1)
-        return result
-
-    return await asyncio.gather(*map(mapper, urls))
+locals().update(**{
+    method : partial(request, method)
+    for method in ['get', 'post', 'put']
+})
 
 
 @agnostic
