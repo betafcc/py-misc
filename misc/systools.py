401b1420a86f35b9e8f9f50fec64a06fcccc4ce5
Beta Faccion <betafcc@gmail.com>
2018-10-17 15:07:03 -0300

    Adds systools.lines

--- misc/systools.py
+++ misc/systools.py
@@ -1,10 +1,20 @@
-import io
-import subprocess
+import os
+from subprocess import Popen, PIPE
 
 
-def shell(cmd, *args, **kwargs):
-    return io.TextIOWrapper(
-        subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout,
-        *args,
-        **kwargs,
-    )
+def lines(cmd):
+    '''
+    Iterate shell comand output by line
+    '''
+    with Popen(
+        cmd,
+        shell=True,
+        text=True,
+        bufsize=1,
+        stdout=PIPE,
+    ) as process:
+        yield from map(process_line, process.stdout)
+
+
+def process_line(line):
+    return line[:-1] if line[-1] == os.linesep else line
